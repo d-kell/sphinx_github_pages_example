cf5721835a0db3e3ddaba04f97842822b8e866aa
Dan Kelliher <dan.kelliher@gmail.com>
2020-07-19 17:01:49 -0700

    adding functions to document

---
+++ example.py
@@ -0,0 +1,18 @@
+def func1(arg1, arg2):
+    """
+    Test function for demo purposes
+    :param arg1: arg1
+    :param arg2: arg2
+
+    :return: returns arg1
+    """
+    arg1 = arg2
+    return arg 1
+
+def func2():
+    """
+    Test function for demo purposes
+
+    :return:
+    """
+    return none
